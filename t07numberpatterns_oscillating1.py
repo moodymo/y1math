63afd5f40e991b1277d521981a63bbcd615c9dd6
moodymo <53069916+moodymo@users.noreply.github.com>
2019-10-24 14:36:35 +0800

    Update t07numberpatterns_oscillating1.py

--- t07numberpatterns_oscillating1.py
+++ t07numberpatterns_oscillating1.py
@@ -2,5 +2,15 @@
 
 k = 10
 
-for i in range(k):
+for i in range(1,k+1):
+  if i%2==0:
+    if i==k:
+      print("-1", end=" ")
+    else:
+      print("-1", end=", ")
+  else:
+    if i==k:
+      print("-1", end=" ")
+    else:
+      print("1", end=", ")
   
